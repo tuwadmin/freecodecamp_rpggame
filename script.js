defe60f907d9be319e8c873c99779580fb94539d
Samia Lemfadli <50528819+tuwadmin@users.noreply.github.com>
2025-08-12 15:12:15 +0000

    updated buyweapon function

--- script.js
+++ script.js
@@ -93,7 +93,9 @@ function buyWeapon() {
     let newWeapon = weapons[currentWeaponIndex].name;
     text.innerText = "You now have a " + newWeapon + ".";
     inventory.push(newWeapon);
-    
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "You do not have enough gold to buy a weapon.";
   }
 }
 
