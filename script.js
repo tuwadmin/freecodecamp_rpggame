e36dc5d990e4f7a7f5043a4e1fac6537b5d9e866
Samia Lemfadli <50528819+tuwadmin@users.noreply.github.com>
2025-08-28 16:11:19 +0000

    added attack functionality

--- script.js
+++ script.js
@@ -160,7 +160,11 @@ function goFight() {
 }
 
 function attack() {
-
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
+  health -= monsters[fighting].level;
+  monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  
 }
 
 function dodge() {
